a90793ef484aaadd55f646d24125b59cf2bdc126
Cannaset <ktoki427@gmail.com>
2026-05-02 16:09:14 +0800

    ngerapiin folder

---
+++ App/JS/script-login.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#loginForm");
+    const nim = document.querySelector("#nim");
+    const password = document.querySelector("#password");
+    const errorMessage = document.querySelector("#error-message");
+
+    const correctNim = "2310110001";
+    const correctPassword = "123456";
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const userNim = nim.value.trim();
+        const userPassword = password.value.trim();
+
+        if (userNim === "" && userPassword === "") {
+            showMessage("NIM dan Password wajib diisi");
+            return;
+        }
+
+        if (userNim === "") {
+            showMessage("NIM tidak boleh kosong");
+            return;
+        }
+
+        if (userPassword === "") {
+            showMessage("Password tidak boleh kosong");
+            return;
+        }
+
+        if (userNim !== correctNim) {
+            showMessage("NIM tidak ditemukan");
+            return;
+        }
+
+        if (userPassword !== correctPassword) {
+            showMessage("Password salah");
+            return;
+        }
+
+        showMessage("Login berhasil");
+        errorMessage.style.color = "green";
+
+        setTimeout(function () {
+            window.location.href = "feed.html";
+        }, 1000);
+    });
+
+    function showMessage(text) {
+        errorMessage.innerHTML = text;
+        errorMessage.style.color = "#ef4444";
+    }
+});
