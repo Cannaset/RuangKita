f6f7f370c506ce11b3b251931f255d44e67bf804
Cannaset <ktoki427@gmail.com>
2026-05-05 10:44:34 +0800

    ketinggalan

---
+++ App/config/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbName = 'pemweb_project';
+$username = 'root';
+$password = '';
+
+$conn = new mysqli($host, $username, $password, $dbName);
+
+if ($conn->connect_error) {
+    die('Koneksi database gagal: ' . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
